6138621e3ed718d4b68729b24fc60fefcacdfb92
adhokshaj <aadhok2@gmail.com>
2026-07-14 10:45:43 -0700

    Add: longest-common-subsequence - submission-1

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-1.py
@@ -0,0 +1,21 @@
+from functools import cache
+class Solution:
+    def longestCommonSubsequence(self, s: str, t: str) -> int:
+        n,m = len(s), len(t)
+        @cache
+        def dfs(i,j):
+            if i==n or j==m:
+                return 0
+            
+            #take
+            take = 0
+            if s[i]==t[j]:
+                take = 1 + dfs(i+1,j+1)
+
+            #dont_take
+            dont_take = max(dfs(i+1,j), dfs(i,j+1))
+
+            return max(take, dont_take)
+        
+        return dfs(0,0)
+        
