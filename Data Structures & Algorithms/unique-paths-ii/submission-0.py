1c1cdb52abfa40d5f16e2dac0831c16375d589e8
adhokshaj <aadhok2@gmail.com>
2026-09-02 12:59:07 -0400

    Add: unique-paths-ii - submission-0

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-0.py
@@ -0,0 +1,14 @@
+from functools import cache
+class Solution:
+    def uniquePathsWithObstacles(self, grid: List[List[int]]) -> int:
+        n,m = len(grid), len(grid[0])
+        
+        @cache
+        def dfs(i,j):
+            if i==n or j==m or grid[i][j]==1:
+                return 0
+            if i==n-1 and j==m-1:
+                return 1
+            
+            return dfs(i+1,j) + dfs(i,j+1)
+        return dfs(0,0)
